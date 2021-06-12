ead8be9ecceef86a4c4d6adabe08f69e807be656
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-12 21:41:09 +0530

    Added Dynamic binding

--- LwCPlayground/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ LwCPlayground/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+    bindingElement = ""
+    addValue(event){
+        // console.log(event.detail.value)
+        this.bindingElement = event.detail.value;
+    }
+}
