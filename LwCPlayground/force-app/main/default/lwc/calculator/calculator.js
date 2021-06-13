e0b9fb949cfbf76fbf8c30dd4ed337beefc27c2b
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-13 13:00:04 +0530

    Dummy logic for math in js

--- LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
+++ LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
@@ -1,20 +1,28 @@
 import { LightningElement } from 'lwc';
 
 export default class Calculator extends LightningElement {
-    
+    input1(event){
+        v1 = event.detail.value
+    }
+    input2(event){
+        v2 = event.target.value
+    }
     
     handleAdd(event){
-        console.log(event.target)
+        console.log(v1+v2);
     }
     
     handleSub(event){
-        console.log(event.target)
+        console.log(v1+v2);
+        
     }
     
     handleMul(event){
-        console.log(event.target)
+        console.log(v1+v2);
+        
     }
     handleDiv(event){
-        console.log(event.target)
+        console.log(v1+v2);
+        
     }
 }
