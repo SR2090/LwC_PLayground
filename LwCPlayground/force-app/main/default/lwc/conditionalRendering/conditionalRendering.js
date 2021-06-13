000c991f05460f75e03870641e1c8e58a6f85136
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-13 08:38:29 +0530

    eventhandler

--- LwCPlayground/force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
+++ LwCPlayground/force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
 
-export default class ConditionalRendering extends LightningElement {}
+export default class ConditionalRendering extends LightningElement {
+    condition = true;
+    handleCheck(event){
+        console.log(event.details.value);
+    }
+}
