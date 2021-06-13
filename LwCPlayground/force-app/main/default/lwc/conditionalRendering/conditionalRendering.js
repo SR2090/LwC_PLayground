527e12d78ade7f9280571bd45905a644c9f22d3a
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-13 08:52:59 +0530

    isExposed:true, added checkbox cond, inline style

--- LwCPlayground/force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
+++ LwCPlayground/force-app/main/default/lwc/conditionalRendering/conditionalRendering.js
@@ -3,6 +3,7 @@ import { LightningElement } from 'lwc';
 export default class ConditionalRendering extends LightningElement {
     condition = true;
     handleCheck(event){
-        console.log(event.details.value);
+        console.log(event.detail.checked);
+        this.condition = event.detail.checked;
     }
 }
