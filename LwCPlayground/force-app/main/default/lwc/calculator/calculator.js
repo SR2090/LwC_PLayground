055c38494a51a74882b23821966a9e1ef587dd57
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-13 12:53:23 +0530

    Added dummy event handler js, inline style markup

--- LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
+++ LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
@@ -1,3 +1,20 @@
 import { LightningElement } from 'lwc';
 
-export default class Calculator extends LightningElement {}
+export default class Calculator extends LightningElement {
+    
+    
+    handleAdd(event){
+        console.log(event.target)
+    }
+    
+    handleSub(event){
+        console.log(event.target)
+    }
+    
+    handleMul(event){
+        console.log(event.target)
+    }
+    handleDiv(event){
+        console.log(event.target)
+    }
+}
