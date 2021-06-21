823f164368499278d49666242ed730d3232e434e
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-21 18:38:58 +0530

    Add handler

--- LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
+++ LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
@@ -3,14 +3,21 @@ import { LightningElement, track } from 'lwc';
 export default class Calculator extends LightningElement {
     Number1;
     Number2;
+    Result;
     inputHandler(event){
         if(event.target.name === "Number1"){
             this.Number1 = event.detail.value;
         }else{
             this.Number2 = event.detail.value;
         }
-        
-        
-        console.log(this.Number1,this.Number2)
     }
+    addHandler(){
+        if(this.Number1 === undefined || this.Number1 === undefined){
+
+        }else{
+            this.Result = parseInt(this.Number1) + parseInt(this.Number2);
+        }
+        console.log(this.Result, typeof(this.Result));
+    }
+    
 }
