da6fd437a4debeafa0f76fc88e31906c8d5575a4
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-21 18:14:13 +0530

    onchng for input and def in js

--- LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
+++ LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
@@ -3,11 +3,9 @@ import { LightningElement, track } from 'lwc';
 export default class Calculator extends LightningElement {
     Number1;
     Number2;
-    input1(event){
-        console.log(event.detail.value);
+    inputHandler(event){
+        this.Number1 = event.detail.value;
+        this.Number2 = event.detail.value;
+        console.log(this.Number1,this.Number2)
     }
-    input2(event){
-        console.log(event.detail.value);
-    }
-    
 }
