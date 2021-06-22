19852d34e3996ce9a1d926d15528d357f86761b9
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-22 10:35:53 +0530

    Add logic and handler for cond and chk box

--- LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
+++ LwCPlayground/force-app/main/default/lwc/calculator/calculator.js
@@ -1,10 +1,11 @@
 import { LightningElement, track } from 'lwc';
-import {Stack} from 'stackJs.js';
+
 export default class Calculator extends LightningElement {
     Number1;
     Number2;
     Result = "";
-    Queue = [];
+    Stack = [];
+    status = true;
     inputHandler(event){
         if(event.target.name === "Number1"){
             this.Number1 = event.detail.value;
@@ -16,15 +17,16 @@ export default class Calculator extends LightningElement {
         if(this.Number1 === undefined || this.Number1 === undefined){
 
         }else{
-            this.Result = `${parseInt(this.Number1) + parseInt(this.Number2)}`;
+            this.Result = `Result of Adding ${this.Number1} and ${this.Number2} is ${parseInt(this.Number1) + parseInt(this.Number2)}`;
         }
+        this.Stack.push(this.Result);
         console.log(this.Result, typeof(this.Result));
     }
     subHandler(){
         if(this.Number1 === undefined || this.Number1 === undefined){
 
         }else{
-            this.Result = `${parseInt(this.Number1) - parseInt(this.Number2)}`;
+            this.Result = `Result of Subtracting ${this.Number2} from ${this.Number2} is ${parseInt(this.Number1) - parseInt(this.Number2)}`;
         }
         console.log(this.Result, typeof(this.Result));
     }
@@ -32,7 +34,7 @@ export default class Calculator extends LightningElement {
         if(this.Number1 === undefined || this.Number1 === undefined){
 
         }else{
-            this.Result = `${parseInt(this.Number1) * parseInt(this.Number2)}`;
+            this.Result = `Result of Multiplying ${this.Number1} and ${this.Number2} is ${parseInt(this.Number1) * parseInt(this.Number2)}`;
         }
         console.log(this.Result, typeof(this.Result));
     }
@@ -40,12 +42,14 @@ export default class Calculator extends LightningElement {
         if(this.Number1 === undefined || this.Number1 === undefined){
 
         }else{
-            this.Result = `${parseInt(this.Number1) / parseInt(this.Number2)}`;
+            this.Result = `Result of Divding ${this.Number1} from ${this.Number2} is ${parseInt(this.Number1) / parseInt(this.Number2)}`;
         }
         console.log(this.Result, typeof(this.Result));
     }
-    showHistory(){
-
+    showHistory(event){
+        status = event.detail.checked;  
+        console.log(status)    
     }
+
     
 }
