9914700aa84faf5d9f6f56b6b6f81914dcb005a0
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-29 23:12:24 +0530

    Handled input from fields

--- LwCPlayground/force-app/main/default/lwc/bMICalculator/bMICalculator.js
+++ LwCPlayground/force-app/main/default/lwc/bMICalculator/bMICalculator.js
@@ -2,6 +2,16 @@ import { LightningElement } from 'lwc';
 
 export default class BMICalculator extends LightningElement {
     titleForLwc = "BMI Calculator 12";
+    Height;
+    Weight;
+    getValues(event){
+        if(event.target.name === "Height"){
+            console.log(event.detail.value);
+        }
+        if(event.target.name === "Weight"){
+            console.log(event.detail.value);
+        }
+    }
     handleClick(event){
         this.titleForLwc = "Changed Title";
         console.log("value of title", this.titleForLwc);
