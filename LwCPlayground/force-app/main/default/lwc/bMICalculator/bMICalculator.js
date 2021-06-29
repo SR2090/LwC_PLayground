7ad3514b96bab4bdebd926981f239a5fbc6b6ad4
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-29 23:44:29 +0530

    getting value from input parseFloat

--- LwCPlayground/force-app/main/default/lwc/bMICalculator/bMICalculator.js
+++ LwCPlayground/force-app/main/default/lwc/bMICalculator/bMICalculator.js
@@ -1,19 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,  } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class BMICalculator extends LightningElement {
     titleForLwc = "BMI Calculator 12";
     Height;
     Weight;
+    btnView = false;
+    Result;
     getValues(event){
         if(event.target.name === "Height"){
-            console.log(event.detail.value);
+            this.Height = parseFloat(event.detail.value);
+            // console.log(event.detail.value);
         }
         if(event.target.name === "Weight"){
+            this.Weight= parseFloat(event.detail.value);
             console.log(event.detail.value);
         }
     }
+   
     handleClick(event){
-        this.titleForLwc = "Changed Title";
-        console.log("value of title", this.titleForLwc);
+        this.btnView = true;
+        const evt = new ShowToastEvent({
+            title: 'Blank input',
+            message: 'Please enter input values',
+            variant: 'error',
+            mode: 'dismissable'
+        });
+        // if(typeof this.Height == "undefined" || typeof this.Weight == "undefined"){
+        //     this.dispatchEvent(evt);  
+        //     console.log(this.Height, this.Weight);       
+        // }else{
+            
+            this.Result = ( parseFloat(this.Weight)/ (parseFloat(this.Height) * parseFloat(this.Height))).toPrecision(2);
+            console.log(this.Result)
+        // }
+        // console.log(this.Height);   
+        // console.log(event.detail.value);
     }
 }
