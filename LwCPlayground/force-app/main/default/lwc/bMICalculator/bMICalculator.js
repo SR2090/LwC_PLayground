6321188873ec1e40950cd1feb6e18fe6af363a19
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-06-29 10:39:42 +0530

    Add change event handler for title

--- LwCPlayground/force-app/main/default/lwc/bMICalculator/bMICalculator.js
+++ LwCPlayground/force-app/main/default/lwc/bMICalculator/bMICalculator.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class BMICalculator extends LightningElement {}
+export default class BMICalculator extends LightningElement {
+    titleForLwc = "BMI Calculator 12";
+    handleClick(event){
+        this.titleForLwc = "Changed Title";
+        console.log("value of title", this.titleForLwc);
+    }
+}
