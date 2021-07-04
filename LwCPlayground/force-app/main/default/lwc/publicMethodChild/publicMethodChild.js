e36fbc904a12815f364517dd802fcfcfdd737775
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-04 12:30:46 +0530

    Removed handle change

--- LwCPlayground/force-app/main/default/lwc/publicMethodChild/publicMethodChild.js
+++ LwCPlayground/force-app/main/default/lwc/publicMethodChild/publicMethodChild.js
@@ -1,20 +1,24 @@
 import { LightningElement, track } from 'lwc';
 
 export default class PublicMethodChild extends LightningElement {
-    @track value = [];
+    @track value = ['red'];
 
     get options() {
         return [
-            { label: 'Ross', value: 'option1' },
-            { label: 'Rachel', value: 'option2' },
+            { label: 'Red', value: 'red' },
+            { label: 'Blue', value: 'blue' },
+            { label: 'Green', value: 'green' },
+            { label: 'Orange', value: 'orange' },
+            { label: 'Pink', value: 'pink' },
         ];
     }
 
     get selectedValues() {
         return this.value.join(',');
     }
-
-    handleChange(e) {
-        this.value = e.detail.value;
-    }
+    // Handle change is removed as the checkbox will be selected from
+    // the input value in the parent component ( It handler will set the value)
+    // handleChange(e) {
+    //     this.value = e.detail.value;
+    // }
 }
