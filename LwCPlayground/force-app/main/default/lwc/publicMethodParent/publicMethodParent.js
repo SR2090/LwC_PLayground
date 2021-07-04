6aad44b3c3eba56dec25071ecee6dc202b990256
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-04 13:05:05 +0530

    calling child public property

--- LwCPlayground/force-app/main/default/lwc/publicMethodParent/publicMethodParent.js
+++ LwCPlayground/force-app/main/default/lwc/publicMethodParent/publicMethodParent.js
@@ -1,5 +1,15 @@
 import { LightningElement } from 'lwc';
 
 export default class PublicMethodParent extends LightningElement {
-    
+    inputVal;
+    // after calling the child component in the 
+    // template we need to 
+    // call the public method
+    setCheckBox(event){
+        // calling the child component's public method has 2 steps
+        // first i need to select the child component using document.querySelector
+        const childComponent = document.querySelector('c-public-method-child');
+        // then i need to call the child component method
+        childComponent.selectCheckBox(this.inputVal);
+    }    
 }
