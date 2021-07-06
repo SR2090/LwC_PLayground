672da62315c6f6dcc69796a619bf156dea9718a8
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-06 12:36:29 +0530

    defined functions

--- LwCPlayground/force-app/main/default/lwc/lifeCycleDemo/lifeCycleDemo.js
+++ LwCPlayground/force-app/main/default/lwc/lifeCycleDemo/lifeCycleDemo.js
@@ -1,5 +1,25 @@
 import { LightningElement } from 'lwc';
 
 export default class LifeCycleDemo extends LightningElement {
+    LifeCycleDemo(){
+        console.log("-----------------------------------------------------")
+        console.log("Constructor is called since the component is called");
+        console.log("-----------------------------------------------------")
+    }
     
+    connectedCallback(){
+        console.log("-----------------------------------------------------")
+        console.log("Component has been inserted into the dom USE TO MANIPULATE Attr.")
+        console.log("-----------------------------------------------------")
+    }
+    renderedCallback(){
+        console.log("-----------------------------------------------------")
+        console.log("Component has been rendered. The DOM can be rendered Programitically")
+        console.log("-----------------------------------------------------")
+    }
+    disconnectedCallback(){
+        console.log("#####################################################")
+        console.log("User has changed page component is DYINGG ARGHHHHHHHHHHHHH.......")
+        console.log("#####################################################")
+    }
 }
