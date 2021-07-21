8a29e0bea3ce6083146675b5de0e3a3b6a380b32
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-21 19:35:28 +0530

    Practice lwc loop and table

---
+++ LwCPlayground/force-app/main/default/lwc/meetingRooms_2/meetingRooms_2.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+
+export default class MeetingRooms extends LightningElement {
+    MeetingRooms = [
+        {roomName:'A-01', roomCapacity:'10'},
+        {roomName:'A-02', roomCapacity:'11'},
+        {roomName:'A-03', roomCapacity:'13'},
+        {roomName:'A-04', roomCapacity:'13'},
+        {roomName:'B-01', roomCapacity:'120'},
+        {roomName:'B-02', roomCapacity:'221'},
+        {roomName:'B-03', roomCapacity:'122'},
+        {roomName:'C-01', roomCapacity:'10'},
+        {roomName:'C-02', roomCapacity:'201'},
+        {roomName:'D-01', roomCapacity:'500'},
+        {roomName:'D-02', roomCapacity:'501'}
+    ]
+}
