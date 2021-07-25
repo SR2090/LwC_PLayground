1fc230b00f8c71f29dd8ab838b859d6c1de64b1a
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-25 10:41:30 +0530

    BMI helper params corrected works

--- LwCPlayground/force-app/main/default/lwc/bmIHelper/bmIHelper.js
+++ LwCPlayground/force-app/main/default/lwc/bmIHelper/bmIHelper.js
@@ -1,5 +1,13 @@
-import { LightningElement } from 'lwc';
-
-export default class BmIHelper extends LightningElement {
-    
+const bmiHelperFunction = (Height, Weight) => {
+    if(typeof Height == "undefined" || typeof Weight == "undefined"){
+        // this.dispatchEvent(evt);    
+        return undefined;
+    }else{
+        let temp = Height * Height;
+        let deno = Weight;
+        let res = deno / temp
+        return res.toPrecision(2);
+    }
 }
+
+export {bmiHelperFunction};
