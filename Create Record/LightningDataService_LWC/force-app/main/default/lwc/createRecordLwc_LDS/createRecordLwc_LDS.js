41af7c2d4fa429796bcaaeb10de399b1339f42e4
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-31 18:19:12 +0530

    Logic to create and save account finished

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/createRecordLwc_LDS/createRecordLwc_LDS.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/createRecordLwc_LDS/createRecordLwc_LDS.js
@@ -1,9 +1,9 @@
 import { LightningElement } from 'lwc';
-
+import { createRecord } from 'lightning/uiRecordApi';
 export default class CreateRecordLwc_LDS extends LightningElement {
     Name;
     PhNumber;
-    Email;
+    Website;
     accountNameChangeHandler(event){
         this.Name = event.detail.value;
     }
@@ -11,6 +11,30 @@ export default class CreateRecordLwc_LDS extends LightningElement {
         this.PhNumber = event.detail.value;
     }
     accountWebsiteChangeHandler(event){
-        this.Email = event.detail.value;
+        this.Website = event.detail.value;
+    }
+    // createAccount method to map field values params with actual name 
+    createAccount(){
+        // takes
+        // field apiName as key 
+        // value as value
+        const fields = {
+            "Name" : this.Name,
+            "Phone" : this.PhNumber,
+            "purchase_department_email__c": this.Website,
+        };
+        // Create a Js object to map these fields to their object name
+        // takes an object name
+        // Fields 
+        // is an input for createRecord method
+        const recordInput = {
+            apiName: 'Account',
+            fields,
+        }
+        createRecord(recordInput).then(response => {
+            console.log('Account has been created ', response.id)
+        }).catch(error => {
+            console.log(error);
+        });
     }
 }
