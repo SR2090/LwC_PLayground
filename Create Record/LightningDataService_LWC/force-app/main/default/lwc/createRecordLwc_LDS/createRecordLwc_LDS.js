58809ac15af6645deaf95f7afd720eb372dadb30
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-07-31 17:47:22 +0530

    Input read

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/createRecordLwc_LDS/createRecordLwc_LDS.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/createRecordLwc_LDS/createRecordLwc_LDS.js
@@ -1,3 +1,16 @@
 import { LightningElement } from 'lwc';
 
-export default class CreateRecordLwc_LDS extends LightningElement {}
+export default class CreateRecordLwc_LDS extends LightningElement {
+    Name;
+    PhNumber;
+    Email;
+    accountNameChangeHandler(event){
+        this.Name = event.detail.value;
+    }
+    accountPhoneNumberChangeHandler(event){
+        this.PhNumber = event.detail.value;
+    }
+    accountWebsiteChangeHandler(event){
+        this.Email = event.detail.value;
+    }
+}
