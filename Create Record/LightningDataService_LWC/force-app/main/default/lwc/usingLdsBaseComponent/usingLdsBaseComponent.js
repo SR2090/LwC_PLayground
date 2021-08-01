8af0f7e628a47c1f2c33fb6481db2760b084ef00
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-01 12:17:31 +0530

    logging recordId and passing it to view-form

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/usingLdsBaseComponent/usingLdsBaseComponent.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/usingLdsBaseComponent/usingLdsBaseComponent.js
@@ -1,3 +1,9 @@
 import { LightningElement } from 'lwc';
 
-export default class UsingLdsBaseComponent extends LightningElement {}
+export default class UsingLdsBaseComponent extends LightningElement {
+    recordId;
+    SuccessHandler(event){
+        this.recordId = event.detail.id;
+        console.log(this.recordId);
+    }
+}
