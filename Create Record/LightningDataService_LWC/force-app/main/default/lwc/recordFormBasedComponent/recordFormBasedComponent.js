ddb04e6fadf249ea5e5906c39d8193f8068cd67a
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-02 11:18:14 +0530

    removed fields

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/recordFormBasedComponent/recordFormBasedComponent.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/recordFormBasedComponent/recordFormBasedComponent.js
@@ -1,14 +1,14 @@
 import { LightningElement } from 'lwc';
 
-import NAME_FIELDS from '@salesforce/schema/Account.Name';
-import PH_FIELDS from '@salesforce/schema/Account.Name';
-import WEBSTE_FIELDS from '@salesforce/schema/Account.Name';
+// import NAME_FIELDS from '@salesforce/schema/Account.Name';
+// import PH_FIELDS from '@salesforce/schema/Account.Name';
+// import WEBSTE_FIELDS from '@salesforce/schema/Account.Name';
 
 export default class RecordFormBasedComponent extends LightningElement {
     recordId;
     // when record Id is null as in when we are first loading the component
     // the form will be rendered in create mode.
-    fieldsArray = [NAME_FIELDS, PH_FIELDS, WEBSTE_FIELDS];
+    // fieldsArray = [NAME_FIELDS, PH_FIELDS, WEBSTE_FIELDS];
     handleSuccess(event){
         this.recordId = event.detail.recordId;
     }
