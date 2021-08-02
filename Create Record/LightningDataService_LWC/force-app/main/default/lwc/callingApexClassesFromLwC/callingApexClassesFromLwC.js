15a3d05e229d47133fd2cbed4631d3325aa3ff4d
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-02 11:43:58 +0530

    isAccountResolved true false

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/callingApexClassesFromLwC/callingApexClassesFromLwC.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/callingApexClassesFromLwC/callingApexClassesFromLwC.js
@@ -1,3 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import getAccounts from "@salesforce/apex/AccountManger"
+export default class CallingApexClassesFromLwC extends LightningElement {
+    
+    // gets the accounts from the apex class method getAccounts
+    @wire(getAccounts) accounts;
 
-export default class CallingApexClassesFromLwC extends LightningElement {}
+    isAccountsResolved(){
+        if(this.accounts){
+            return true;
+        }
+        return false;
+    }
+}
