dd6d5d861b8732ccaddb1cb4130c1859b6688dfb
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-02 10:55:46 +0530

    saved recordId to chn form from create 2 view/edit

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/recordFormBasedComponent/recordFormBasedComponent.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/recordFormBasedComponent/recordFormBasedComponent.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class RecordFormBasedComponent extends LightningElement {}
+export default class RecordFormBasedComponent extends LightningElement {
+    recordId;
+    // when record Id is null as in when we are first loading the component
+    // the form will be rendered in create mode.
+    fieldsArray = ['Name', 'Phone', 'Website'];
+    handleSuccess(event){
+        this.recordId = event.detail.recordId;
+    }
+}
