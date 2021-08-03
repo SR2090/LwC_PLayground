969e1ffbd44b9175bb20df457e2033ead793f556
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-03 10:42:53 +0530

    added the logic

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/imperitiveApexClassCall/imperitiveApexClassCall.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/imperitiveApexClassCall/imperitiveApexClassCall.js
@@ -1,5 +1,26 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track} from 'lwc';
 import gettheAccounts from '@salesforce/apex/AccountManagerNonCacheable.getAccounts'
 export default class ImperitiveApexClassCall extends LightningElement {
-    
+    number;
+    @track accounts;
+    getAccountNumber(event){
+        this.number = event.target.value;
+    }
+    isAccountsResolved(){
+        if(this.accounts){
+            return true;
+        }
+        return false;
+    }
+    getAccounts(){
+        gettheAccounts({numberOfAccounts: this.number}).then(
+            response => {
+                this.accounts = response;
+            }
+        ).catch(
+            error =>{
+                console.log("An error has occured ", error.body.message )
+            }
+        );
+    }
 }
