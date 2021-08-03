765b8e2bae2c6e815897e7ccd438b15a95949b25
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-03 10:45:13 +0530

    added comments for clarity

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/imperitiveApexClassCall/imperitiveApexClassCall.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/imperitiveApexClassCall/imperitiveApexClassCall.js
@@ -3,9 +3,12 @@ import gettheAccounts from '@salesforce/apex/AccountManagerNonCacheable.getAccou
 export default class ImperitiveApexClassCall extends LightningElement {
     number;
     @track accounts;
+    // gets the number of accounts to retrieve 
+    // passes this as arg in the method call
     getAccountNumber(event){
         this.number = event.target.value;
     }
+    // checks if the accounts are present or not
     isAccountsResolved(){
         if(this.accounts){
             return true;
@@ -13,6 +16,8 @@ export default class ImperitiveApexClassCall extends LightningElement {
         return false;
     }
     getAccounts(){
+        // Promise call to apex class method
+        // JSON body parameters are passed
         gettheAccounts({numberOfAccounts: this.number}).then(
             response => {
                 this.accounts = response;
