5614f8f033221099f47ebda2b82cab0bfed21e48
SR2090 <72628350+SR2090@users.noreply.github.com>
2021-08-03 10:06:50 +0530

    wire service class method import

--- Create Record/LightningDataService_LWC/force-app/main/default/lwc/imperitiveApexClassCall/imperitiveApexClassCall.js
+++ Create Record/LightningDataService_LWC/force-app/main/default/lwc/imperitiveApexClassCall/imperitiveApexClassCall.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc';
-
-export default class ImperitiveApexClassCall extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import gettheAccounts from '@salesforce/apex/AccountManagerNonCacheable.getAccounts'
+export default class ImperitiveApexClassCall extends LightningElement {
+    
+}
